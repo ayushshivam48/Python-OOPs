065cbc3731f2c490995de9c29c46b46de5c4f01f
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2025-03-18 06:33:17 +0530

    Add files via upload

---
+++ PropertyDecorator.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, phy, chem, maths):
+        self.phy = phy
+        self.chem = chem
+        self.maths = maths
+        
+    @property
+    def percentage(self):
+        return str((self.phy + self.chem + self.maths)/3) + "%"
+    
+stu1 = Student(98, 97, 96)
+print(stu1.percentage)
+
+stu1.phy = 89
+print(stu1.percentage)
